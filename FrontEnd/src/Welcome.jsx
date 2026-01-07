732e433109f0587d5cf0e9a49e27d9d238bb6fd2
yash2106-byte <rajyash2510@gmail.com>
2026-01-07 23:56:07 +0530

    installed login photo

---
+++ FrontEnd/src/Welcome.jsx
@@ -0,0 +1,9 @@
+function Welcome() {
+  return (
+    <>
+      <img className="login_image" src="/images/login_background.jpg" alt="login background" />
+    </>
+  );
+}
+
+export default Welcome;
