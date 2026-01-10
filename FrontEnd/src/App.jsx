1562f2fee19cf4682cb239a1bdfe222313bc7308
yash2106-byte <rajyash2510@gmail.com>
2026-01-10 17:18:17 +0530

    sorted the path

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -13,24 +13,48 @@ function App() {
     const [result, setResult] = useState(null);
 
     // This function is called when the user clicks "Analyze"
+
+    // this is the place where the input data are being recived 
     const handleAnalyze = async (promptText) => {
         setIsAnalyzing(true);
         setResult(null); // Clear previous result
 
         // Simulate an API call (waiting 1.5 seconds)
         // Later, we will replace this with a real fetch() to your Node.js backend!
-        setTimeout(() => {
-            setResult({
-                score: 85,
-                feedback: [
-                    "Good clarity on the main objective.",
-                    "Could be more specific about the tone (e.g., 'professional' vs 'casual').",
-                    "Adding a constraint like 'under 50 words' would help precision."
-                ],
-                improvedPrompt: `${promptText} Ensure the tone is professional and concise, keeping the response under 50 words.`
-            });
-            setIsAnalyzing(false);
-        }, 1500);
+        //this set timeout is simulating the backend so for now we will comment this
+        // setTimeout(() => {
+        //     //this is the place where the output is generated
+        //     setResult({
+        //         score: 85,
+        //         feedback: [
+        //             "Good clarity on the main objective.",
+        //             "Could be more specific about the tone (e.g., 'professional' vs 'casual').",
+        //             "Adding a constraint like 'under 50 words' would help precision."
+        //         ],
+        //         improvedPrompt: `${promptText} this is  Ensure the tone is professional and concise, keeping the response under 50 words.`
+        //     });
+        //     setIsAnalyzing(false);
+        // }, 1500);
+        // this thing is working fine with all the things
+        try {
+        const response = await fetch("http://localhost:5000/analyze", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                promptText: promptText, // ⬅️ SENT TO BACKEND
+            }),
+        });
+
+        const data = await response.json();
+
+        setResult(data); // ⬅️ DATA COMING FROM BACKEND
+    } catch (error) {
+        console.error("Error:", error);
+    } finally {
+        setIsAnalyzing(false);
+    }
     };
 
     return (
