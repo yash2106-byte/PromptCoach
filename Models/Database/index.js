5557777954e89958af4be06914777adf8921b622
yash2106-byte <rajyash2510@gmail.com>
2026-01-29 19:02:05 +0530

    fixed database

--- Models/Database/index.js
+++ Models/Database/index.js
@@ -1,6 +1,14 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
+import pg from 'pg';
 
-const db = drizzle(process.env.DATABASE_URL);
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false  // Required for Supabase
+  }
+});
 
-export default db
+const db = drizzle(pool);
+
+export default db;
